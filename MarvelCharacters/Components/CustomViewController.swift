2a4e21c7cab53cdf47189900e32d7cb817ab2b7a
Tales Lemes <tales.lemes@gmail.com>
2020-05-24 16:01:38 -0300

    Implement Character Details Scene Fake Layout (missing table view)

---
+++ MarvelCharacters/Components/CustomViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomViewController.swift
+//  MarvelCharacters
+//
+//  Created by Tales Lemes on 24/05/20.
+//  Copyright © 2020 Tales Lemes. All rights reserved.
+//
+
+import Foundation
